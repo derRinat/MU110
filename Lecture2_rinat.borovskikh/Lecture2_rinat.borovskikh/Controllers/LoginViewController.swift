31e84c778ce99ac1a8f78acf297922c0b27f7af6
rinat.borovskikh <1daylife@gmail.com>
2014-09-08 02:39:38 +0200

    Home task, lecture 2

---
+++ Lecture2_rinat.borovskikh/Lecture2_rinat.borovskikh/Controllers/LoginViewController.swift
@@ -0,0 +1,46 @@
+//
+//  LoginViewController.swift
+//  Lecture2_rinat.borovskikh
+//
+//  Created by Rinat Borovskikh on 08.09.14.
+//  Copyright (c) 2014 rinat.borovskikh. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+  
+    @IBAction func loginAction(sender: UIButton) {
+        
+        // Define alert action
+        
+        var firstAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, {
+            UIAlertAction in
+            println("OK pressed")
+        })
+        
+        // Init alert
+        var alert = UIAlertController(title: "Error", message: "Service unavailable, please try later", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        // Add action
+        alert.addAction(firstAction)
+        
+        // Place in on scene...
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+        //navigationController.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+}
